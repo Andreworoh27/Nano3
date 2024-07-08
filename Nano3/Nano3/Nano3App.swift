eebdc117ee14d10bddd192951880e9ad1a5c7e4b
Andrew Oroh <andreworoh.ao@gmail.com>
2024-07-08 15:59:13 +0700

    Initial Project for Nano Challenge 3

---
+++ Nano3/Nano3/Nano3App.swift
@@ -0,0 +1,17 @@
+//
+//  Nano3App.swift
+//  Nano3
+//
+//  Created by Andrew Oroh on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Nano3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
