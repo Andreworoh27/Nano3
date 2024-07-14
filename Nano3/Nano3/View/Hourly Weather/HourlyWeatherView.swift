8ed0b699d35e3045ee98648715b33d2dd58a865f
RyanKusnadi2023 <138027129+RyanKusnadi2023@users.noreply.github.com>
2024-07-14 15:22:51 +0700

    Revert "Ryan"

--- Nano3/Nano3/View/Hourly Weather/HourlyWeatherView.swift
+++ Nano3/Nano3/View/Hourly Weather/HourlyWeatherView.swift
@@ -4,15 +4,13 @@
 //
 //  Created by Paulus Michael on 12/07/24.
 //
-
 import SwiftUI
 
 struct HourlyWeatherView: View {
-    let hourlyWeatherData: [HourlyWeather] = [
-        HourlyWeather(parsedData: ParsedHourlyWeatherData(time: Calendar.current.date(bySettingHour: 6, minute: 0, second: 0, of: Date())!, precipitation: .none, uvi: 2)),
-        HourlyWeather(parsedData: ParsedHourlyWeatherData(time: Calendar.current.date(bySettingHour: 7, minute: 0, second: 0, of: Date())!, precipitation: .partialCloud, uvi: 3)),
-        HourlyWeather(parsedData: ParsedHourlyWeatherData(time: Calendar.current.date(bySettingHour: 8, minute: 0, second: 0, of: Date())!, precipitation: .cloudy, uvi: 5))
-        // Add more data as needed
+    let hourlyWeatherData: [HourlyWeatherModel] = [
+        HourlyWeatherModel(time: "06:00", uvi: "Low", symbolName: "moon.stars.fill", activitySymbolName: "figure.walk"),
+        HourlyWeatherModel(time: "07:00", uvi: "Low", symbolName: "moon.stars.fill", activitySymbolName: "figure.walk"),
+        HourlyWeatherModel(time: "08:00", uvi: "Low", symbolName: "moon.stars.fill", activitySymbolName: "figure.walk")
     ]
     
     var body: some View {
@@ -26,15 +24,15 @@ struct HourlyWeatherView: View {
             Divider()
                 .background(.white)
             
-            ForEach(Array(hourlyWeatherData.enumerated()), id: \.element.id) { index, weather in
+            ForEach(hourlyWeatherData) { weather in
                 HStack(alignment: .center) {
-                    Text(weather.hour, style: .time)
+                    Text(weather.time)
                         .fontWeight(.semibold)
                         .font(Font.system(size: 22))
                     
                     Spacer()
                     
-                    Image(systemName: weather.precipitation.symbolName)
+                    Image(systemName: weather.symbolName)
                         .imageScale(.large)
                     
                     VStack(alignment: .leading) {
@@ -46,14 +44,12 @@ struct HourlyWeatherView: View {
                     
                     Spacer()
                     
-                    Image(systemName: weather.walkable ? "figure.walk" : "nosign")
+                    Image(systemName: weather.activitySymbolName)
                         .imageScale(.large)
                 }
                 
-                if index < hourlyWeatherData.count - 1 {
-                    Divider()
-                        .background(.white)
-                }
+                Divider()
+                    .background(.white)
             }
         }
         .padding()
@@ -64,24 +60,7 @@ struct HourlyWeatherView: View {
     }
 }
 
-extension Precipitation {
-    var symbolName: String {
-        switch self {
-        case .none:
-            return "sun.max.fill"
-        case .rain:
-            return "cloud.rain.fill"
-        case .snow:
-            return "snow"
-        case .partialCloud:
-            return "cloud.sun.fill"
-        case .cloudy:
-            return "cloud.fill"
-        }
-    }
-}
-
 #Preview {
-    HourlyWeatherView()
-        .background(.black)
+   HourlyWeatherView()
+      .background(.black)
 }
