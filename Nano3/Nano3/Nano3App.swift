9e93b6049fa90f3671ec78ba52c2e88ed90e6d91
Andrew Oroh <andreworoh.ao@gmail.com>
2024-07-15 18:26:57 +0700

    Edit : Change view model from state object to enviroment object

--- Nano3/Nano3/Nano3App.swift
+++ Nano3/Nano3/Nano3App.swift
@@ -6,12 +6,34 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 @main
 struct Nano3App: App {
+    @StateObject var hourlyWeatherViewModel = HourlyWeatherViewModel.shared
+    @StateObject var dailyWeatherViewModel = DailyWeatherViewModel.shared
+    @StateObject var locationViewModel = LocationViewModel.shared
+    @State var initialLocaiton : CLLocation?
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear(){
+                    Task{
+                        await fetchInitialHourlyForecast()
+                    }
+                }
+                .environmentObject(hourlyWeatherViewModel)
+                .environmentObject(dailyWeatherViewModel)
+        }
+    }
+    
+    func fetchInitialHourlyForecast() async{
+
+        initialLocaiton = locationViewModel.currentUserLocation
+        
+        if(initialLocaiton != nil){
+            await hourlyWeatherViewModel.getHourlyForecast(location: locationViewModel.currentUserLocation!, date: Date.now)
         }
     }
 }
