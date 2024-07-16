e175c31ae936f627b74224e4f385ca0c7049c199
RyanKusnadi2023 <ryankusnadi2002@gmail.com>
2024-07-16 13:48:31 +0700

    Edit: DailyWeatherView and ViewModel - Refactor Date Selection

--- Nano3/Nano3/View/Daily Weather/Root/DailyWeatherView.swift
+++ Nano3/Nano3/View/Daily Weather/Root/DailyWeatherView.swift
@@ -1,78 +1,61 @@
-//
-//  HomepageView.swift
-//  Nano3
-//
-//  Created by Paulus Michael on 12/07/24.
-//
-
 import SwiftUI
 import WeatherKit
 
 struct DailyWeatherView: View {
-   @EnvironmentObject var dailyWeatherViewModel : DailyWeatherViewModel
-   @EnvironmentObject var hourlyWeatherViewModel : HourlyWeatherViewModel
-   @StateObject var locationViewModel = LocationViewModel.shared
-   
-   @EnvironmentObject var currentWeatherViewModel: CurrentWeatherViewModel
-   
-   var body: some View {
-      VStack(alignment: .center){
-         Text("Currently viewing weather for")
-            .foregroundStyle(.white)
-            .font(.caption)
-         
-         Text("\(locationViewModel.cityName)")
-            .fontWeight(.bold)
-            .font(.system(size: 16))
-            .foregroundStyle(.white)
-         
-         ScrollView(.horizontal, showsIndicators: false) {
-            HStack{
-               if let dailyForecast = dailyWeatherViewModel.dailyForecast {
-                  ForEach(dailyForecast, id: \.date){ forecast in
-                     DayIconView(date: forecast.date, symbolName: forecast.symbolName)
-                        .background(dailyWeatherViewModel.areDatesSameDay(hourlyWeatherViewModel.selectedDate!, forecast.date) ? Material.ultraThinMaterial.opacity(1) : Material.thinMaterial.opacity(0))
-                        .clipShape(RoundedRectangle(cornerRadius: 12))
-                        .onTapGesture {
-                           
-                           if(Date.isToday(inputDate: forecast.date)){
-                              DispatchQueue.main.async{
-                                 hourlyWeatherViewModel.selectedDate = Date.now
-                              }
-                              
-                           }else{
-                              DispatchQueue.main.async{
-                                 hourlyWeatherViewModel.selectedDate = forecast.date
-                              }
-                              
-                           }
-                           
-                           if let hourlyForecast = hourlyWeatherViewModel.hourlyForecast {
-                              currentWeatherViewModel.findBestTimeFrame(for: hourlyForecast)
-                           }
+    @EnvironmentObject var dailyWeatherViewModel: DailyWeatherViewModel
+    @EnvironmentObject var hourlyWeatherViewModel: HourlyWeatherViewModel
+    @StateObject var locationViewModel = LocationViewModel.shared
+    @EnvironmentObject var currentWeatherViewModel: CurrentWeatherViewModel
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Text("Currently viewing weather for")
+                .foregroundStyle(.white)
+                .font(.caption)
+            
+            Text("\(locationViewModel.cityName)")
+                .fontWeight(.bold)
+                .font(.system(size: 16))
+                .foregroundStyle(.white)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    if let dailyForecast = dailyWeatherViewModel.dailyForecast {
+                        ForEach(dailyForecast, id: \.date) { forecast in
+                            DayIconView(date: forecast.date, symbolName: forecast.symbolName)
+                                .background(dailyWeatherViewModel.areDatesSameDay(hourlyWeatherViewModel.selectedDate ?? Date(), forecast.date) ? Material.ultraThinMaterial.opacity(1) : Material.thinMaterial.opacity(0))
+                                .clipShape(RoundedRectangle(cornerRadius: 12))
+                                .onTapGesture {
+                                    DispatchQueue.main.async {
+                                        hourlyWeatherViewModel.selectedDate = forecast.date
+                                    }
+                                }
+                                .frame(width: 50)
                         }
-                        .frame(width: 50)
-                     
-                  }
-                  .padding(.vertical, 19)
-                  .foregroundStyle(.white)
-               }
+                        .padding(.vertical, 19)
+                        .foregroundStyle(.white)
+                    }
+                }
             }
-         }
-         .padding(.leading)
-      }
-      .padding([.top])
-      .task {
-         if let currentUserLocation = locationViewModel.currentUserLocation{
-            await dailyWeatherViewModel.getDailyWeatherForecast(location: currentUserLocation)
-         }
-      }
-   }
+            .padding(.leading)
+        }
+        .padding([.top])
+        .task {
+            if let currentUserLocation = locationViewModel.currentUserLocation {
+                await dailyWeatherViewModel.getDailyWeatherForecast(location: currentUserLocation)
+                DispatchQueue.main.async {
+                    if let todayForecast = dailyWeatherViewModel.dailyForecast?.first(where: { Date.isToday(inputDate: $0.date) }) {
+                        hourlyWeatherViewModel.selectedDate = todayForecast.date
+                    }
+                }
+            }
+        }
+    }
 }
 
 #Preview {
-   DailyWeatherView()
-      .environmentObject(HourlyWeatherViewModel.shared)
-      .environmentObject(DailyWeatherViewModel.shared)
-      .background(.black)
+    DailyWeatherView()
+        .environmentObject(HourlyWeatherViewModel.shared)
+        .environmentObject(DailyWeatherViewModel.shared)
+        .background(.black)
 }
